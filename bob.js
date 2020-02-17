20ebc1f3c8d5e2b81069cc3e95ae0c6e52d5cf32
Thomas Cheng <tcheng878@gmail.com>
2020-02-17 13:33:43 -0800

    added bob

---
+++ bob.js
@@ -0,0 +1,17 @@
+var name = "Bob";
+var isRaining = true;
+var hasPet = true;
+var job = "Web Developer";
+var cash = 100;
+var happiness = 50;
+
+if(hasPet == true){
+    cash -= 25;
+    happiness += 50;
+}
+if(isRaining = true){
+    happiness -= 15;
+    job = "Has to work from home"
+}
+console.log("Cash is " + cash);
+console.log("Happiness is " + happiness);
